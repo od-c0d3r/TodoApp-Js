08c6729e43ba58b907e3da6f4f92f392868fbfa7
rashad <omar_rashad@aol.com>
2021-08-01 18:05:00 +0200

    Repo: Init Project structure tree with webpack

---
+++ dist/main.js
@@ -0,0 +1,10 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+var main = document.getElementById('main');
+main.innerHTML = "<h1># Todo list App</h1>";
+/******/ })()
+;
+//# sourceMappingURL=main.js.map
---
+++ src/index.js
@@ -0,0 +1,2 @@
+const main = document.getElementById('main');
+main.innerHTML = "<h1># Todo list App</h1>"
